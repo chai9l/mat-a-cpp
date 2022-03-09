2173a482b7336a99acf190abdfa753f1d30d9234
chai9l <62384385+chai9l@users.noreply.github.com>
2022-03-09 17:38:47 +0200

    Add files via upload

---
+++ mat.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include "mat.hpp"
+
+using namespace std;
+
+const vector<vector<char>> mat_vector;
+
+namespace ariel {
+    string mat(int row, int coll, char pattern, char pattern2) {
+        
+        // if (row%2 == 0 || coll%2 == 0) { throw exception ex
+        // }
+
+        
+        return "";
+    }
+}
---
+++ mat.hpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+namespace ariel
+{
+    string mat(int row, int coll, char pattern, char pattern2);
+}
